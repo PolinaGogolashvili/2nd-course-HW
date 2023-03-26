dd0339f9e8e12232199d6b5f11f8002f9091dd10
твое имя < email>
2023-03-26 20:34:42 +0300

    Added task 4

--- skripts/hw-1.js
+++ skripts/hw-1.js
@@ -1,8 +1,17 @@
 let a = 20;
-alert(a);
+alert(а);
 
 const iphoneYear = 2007;
 alert(iphoneYear);
 
 const jsCreator = 'Брендан Эйх';
 alert(jsCreator);
+
+let c = 10;
+let b = 2;
+alert(c + b);
+alert(c - b);
+alert(c * b);
+alert(c / b);
+
+
