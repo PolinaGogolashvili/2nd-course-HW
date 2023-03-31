03e7b72815ac2f944fbc37870bc5653cadee1d01
твое имя < email>
2023-03-31 13:19:41 +0300

    Added task 2

--- scripts/hw-1.js
+++ scripts/hw-1.js
@@ -1,7 +1,14 @@
-let password = "пароль";
-let result = prompt("Введите пароль");
-if (password = "пароль") {
-  alert("Доступ разрешен");
+// let password = "пароль";
+// let result = prompt("Введите пароль");
+// if (password = "пароль") {
+//   alert("Доступ разрешен");
+// } else {
+//   alert("Пароль введен неправильно");
+// }
+
+let c = 5;
+if (c >= 0) {
+  alert("Верно");
 } else {
-  alert("Пароль введен неправильно");
+  alert("Неверно");
 }
