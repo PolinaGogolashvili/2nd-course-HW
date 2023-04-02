f75dfd271a192b545567125ff35274276bcea844
твое имя < email>
2023-04-02 16:30:47 +0300

    Added task 2

--- scripts/hw-1.js
+++ scripts/hw-1.js
@@ -1,5 +1,11 @@
-let a = 1;
-while (a <= 2) {
-  console.log("Привет!");
-  a++;
+// let a = 1;
+// while (a <= 2) {
+//   console.log("Привет!");
+//   a++;
+// }
+
+let b = 1;
+while (b <= 5) {
+  console.log(b);
+  b++;
 }
