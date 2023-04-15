df462690ebc6d1c248dd010dca2c9a29ef3c2c0f
твое имя < email>
2023-04-15 19:24:30 +0300

    Added 3.1

--- scripts/hw-1.js
+++ scripts/hw-1.js
@@ -10,11 +10,16 @@
 // }
 // console.log(Compare(a, b));
 
-function CheckNumber(a) {
-  if (a % 2 === 0) {
-    return console.log("Число четное");
-  } else {
-    return console.log("Число нечетное");
-  }
+// function CheckNumber(a) {
+//   if (a % 2 === 0) {
+//     return console.log("Число четное");
+//   } else {
+//     return console.log("Число нечетное");
+//   }
+// }
+// CheckNumber(7);
+
+function calc(a) {
+  console.log(a ** 2);
 }
-CheckNumber(7);
+calc(9);
