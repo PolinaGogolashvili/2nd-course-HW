8e4fc2ce2105bf388673944f302a8912450f035d
твое имя < email>
2023-04-15 19:04:25 +0300

    Add task 1

--- scripts/hw-1.js
+++ scripts/hw-1.js
@@ -1,1 +1,11 @@
+let a = prompt("Введите первое число");
+let b = prompt("Введите второе число");
 
+function Compare(a, b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+console.log(Compare(a, b));
