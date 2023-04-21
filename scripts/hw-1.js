e8cd8a07bc8b4fa2548300bb157295bbc60fa199
твое имя < email>
2023-04-21 14:09:34 +0300

    add task 2

--- scripts/hw-1.js
+++ scripts/hw-1.js
@@ -1,10 +1,13 @@
+// const numbers = [1, 5, 4, 10, 0, 3];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] == 0) break;
+//   console.log(numbers[i]);
+// }
+
+
+
+
 const numbers = [1, 5, 4, 10, 0, 3];
 
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] == 0) break;
-  console.log(numbers[i]);
-}
-
-
-
-
+console.log(numbers);
