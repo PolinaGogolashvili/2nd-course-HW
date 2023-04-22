56a4aa4a9335024c6893bac3a94bfab42a102baa
твое имя < email>
2023-04-22 15:37:12 +0300

    Add task 10

--- scripts/hw-1.js
+++ scripts/hw-1.js
@@ -80,9 +80,14 @@
 
 
 
-let array = [[1, 2, 3,],[4, 5, 6]];
-console.log(array.reduce((flat, current) => flat.concat(current, [])));
+// let array = [[1, 2, 3,],[4, 5, 6]];
+// console.log(array.reduce((flat, current) => flat.concat(current, [])));
 
+
+let array = [3, 8, 6, 1, 9, 5, 10, 7, 4, 2];
+for(i = 0; i < (array.length - 1); i++) {
+console.log(array[i] + array[i + 1]);
+}
 
 
 
