894b6aead0d8f7bfe54d6c632ec31479ca377f80
твое имя < email>
2023-05-24 00:32:14 +0300

    task 4

--- scripts/hw-1.js
+++ scripts/hw-1.js
@@ -15,12 +15,16 @@
 
 
 
+
+
 // console.log(Math.floor(32.58884));
-
-
 
 // console.log(Math.ceil(32.58884));
 
+// console.log(Math.round(32.58884));
 
 
-console.log(Math.round(32.58884));
+
+console.log(Math.max(52, 53, 49, 77, 21, 32));
+
+console.log(Math.min(52, 53, 49, 77, 21, 32));
