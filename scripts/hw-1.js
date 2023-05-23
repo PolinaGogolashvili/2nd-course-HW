625ede7be44923fb815e6f3dc54ff5dbae4158ef
твое имя < email>
2023-05-24 00:07:50 +0300

    Add task 2

--- scripts/hw-1.js
+++ scripts/hw-1.js
@@ -1,3 +1,14 @@
-const userString = 'js';
+// const userString = 'js';
 
-console.log(userString.toUpperCase());
+// console.log(userString.toUpperCase());
+
+
+
+const animals = ["Кошка", "Кит", "Комар", "Носорог"];
+
+function searchStart(result, startLetter) {
+
+  return result.map(el => el.toLowerCase()).filter(el => el.startsWith(startLetter));
+}
+
+console.log(searchStart(animals, 'ко'));
