f0e2ea77d55dc709be838e9fb6f7082223e6f3df
твое имя < email>
2023-05-24 20:55:02 +0300

    Add task 11

--- scripts/hw-1.js
+++ scripts/hw-1.js
@@ -50,54 +50,56 @@
 // currentDate.setDate(73);
 // console.log(currentDate);
 
-let days = [
-  "Воскресенье",
-  "Понедельник",
-  "Вторник",
-  "Среда",
-  "Четверг",
-  "Пятница",
-  "Суббота",
-];
-let months = [
-  "Января",
-  "Февраля",
-  "Марта",
-  "Апреля",
-  "Мая",
-  "Июня",
-  "Июля",
-  "Августа",
-  "Сентября",
-  "Октября",
-  "Ноября",
-  "Декабря",
-];
+// let days = [
+//   "Воскресенье",
+//   "Понедельник",
+//   "Вторник",
+//   "Среда",
+//   "Четверг",
+//   "Пятница",
+//   "Суббота",
+// ];
+// let months = [
+//   "Января",
+//   "Февраля",
+//   "Марта",
+//   "Апреля",
+//   "Мая",
+//   "Июня",
+//   "Июля",
+//   "Августа",
+//   "Сентября",
+//   "Октября",
+//   "Ноября",
+//   "Декабря",
+// ];
 
-let currentDate = new Date();
+// let currentDate = new Date();
 
-let fullDate =
-  "Дата : " +
-  currentDate.getDate() +
-  " " +
-  months[currentDate.getMonth()] +
-  " " +
-  currentDate.getFullYear() +
-  " - это " +
-  days[currentDate.getDay()];
+// let fullDate =
+//   "Дата : " +
+//   currentDate.getDate() +
+//   " " +
+//   months[currentDate.getMonth()] +
+//   " " +
+//   currentDate.getFullYear() +
+//   " - это " +
+//   days[currentDate.getDay()];
 
-let hour = currentDate.getHours(); 
-let minute = currentDate.getMinutes();
-let second = currentDate.getSeconds();
+// let hour = currentDate.getHours(); 
+// let minute = currentDate.getMinutes();
+// let second = currentDate.getSeconds();
 
-if (minute < 10) { 
-	minute = "0" + minute;
-}
-if (second < 10) { 
-	second = "0" + second;
-}
+// if (minute < 10) { 
+// 	minute = "0" + minute;
+// }
+// if (second < 10) { 
+// 	second = "0" + second;
+// }
 
-let fullTime = "Время : " + hour + ':' + minute + ':' + second;
+// let fullTime = "Время : " + hour + ':' + minute + ':' + second;
 
-console.log(fullDate);
-console.log(fullTime);
+// console.log(fullDate);
+// console.log(fullTime);
+
+
