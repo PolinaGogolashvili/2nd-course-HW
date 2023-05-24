98fa4ec62e3dea2b15d2b493e477a39f375af8c5
твое имя < email>
2023-05-24 16:15:56 +0300

    Add task 10

--- scripts/hw-1.js
+++ scripts/hw-1.js
@@ -2,8 +2,6 @@
 
 // console.log(userString.toUpperCase());
 
-
-
 // const animals = ["Кошка", "Кит", "Комар", "Носорог"];
 
 // function searchStart(result, startLetter) {
@@ -13,23 +11,15 @@
 
 // console.log(searchStart(animals, 'ко'));
 
-
-
-
-
 // console.log(Math.floor(32.58884));
 
 // console.log(Math.ceil(32.58884));
 
 // console.log(Math.round(32.58884));
 
-
-
 // console.log(Math.max(52, 53, 49, 77, 21, 32));
 
 // console.log(Math.min(52, 53, 49, 77, 21, 32));
-
-
 
 // function getRandomIntInclusive(min, max) {
 //   min = Math.ceil(min);
@@ -37,8 +27,6 @@
 //   return Math.floor(Math.random() * (max - min + 1)) + min;
 // }
 // console.log(getRandomIntInclusive(1, 10));
-
-
 
 // function getRandomNum(maxValue) {
 //   const numbs = [];
@@ -49,22 +37,56 @@
 // }
 // console.log(getRandomNum(10));
 
-
-
 // function getRandomNum(min, max) {
 //   let result =  min + Math.random() * (max + 1 - min);
 //   return Math.floor(result);
 // }
 // console.log(getRandomNum(5, 8));
 
-
-
 // let currentDate = new Date();
 // console.log(currentDate);
 
+// let currentDate = new Date();
+// currentDate.setDate(73);
+// console.log(currentDate);
 
+let days = [
+  "Воскресенье",
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+];
+let months = [
+  "Января",
+  "Февраля",
+  "Марта",
+  "Апреля",
+  "Мая",
+  "Июня",
+  "Июля",
+  "Августа",
+  "Сентября",
+  "Октября",
+  "Ноября",
+  "Декабря",
+];
 
 let currentDate = new Date();
-currentDate.setDate(73);
-console.log(currentDate);
 
+let fullDate =
+  "Дата : " +
+  currentDate.getDate() +
+  " " +
+  months[currentDate.getMonth()] +
+  " " +
+  currentDate.getFullYear() +
+  " - это " +
+  days[currentDate.getDay()]; +
+  "Время : " +
+  currentDate.getHours() +
+  currentDate.getMinutes() +
+  currentDate.getSeconds()
+console.log(fullDate);
