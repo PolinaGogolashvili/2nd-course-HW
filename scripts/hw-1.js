21609f69d16bf91cae5be50d8d008abd0eea99aa
твое имя < email>
2023-05-25 22:16:50 +0300

    task 2

--- scripts/hw-1.js
+++ scripts/hw-1.js
@@ -1,7 +1,32 @@
-const people = [
-  { name: 'Глеб', age: 29 },
-  { name: 'Анна', age: 17 },
-  { name: 'Олег', age: 7 },
-  { name: 'Оксана', age: 47 }
-];
-console.log(people.sort((a, b) => a.age > b.age ? 1 : -1));
+// const people = [
+//   { name: 'Глеб', age: 29 },
+//   { name: 'Анна', age: 17 },
+//   { name: 'Олег', age: 7 },
+//   { name: 'Оксана', age: 47 }
+// ];
+// console.log(people.sort((a, b) => a.age > b.age ? 1 : -1));
+
+
+
+
+
+function isPositive() {
+  // писать код тут
+  }
+  function isMale() {
+  // писать код тут
+  }
+  function filter() {
+  // писать код тут
+  }
+  
+  console.log(filter([3, -4, 1, 9], isPositive));
+
+  const people = [
+    {name: 'Глеб', gender: 'male'},
+    {name: 'Анна', gender: 'female'},
+    {name: 'Олег', gender: 'male'},
+    {name: 'Оксана', gender: 'female'}
+ ];
+ 
+ console.log(filter(people, isMale));
