b7d8006c80a46f7a190086d08d5cf12d5f267cb7
твое имя < email>
2023-05-26 11:36:22 +0300

    Add task 3

--- scripts/hw-1.js
+++ scripts/hw-1.js
@@ -10,33 +10,48 @@
 
 
 
-function isPositive(number) {
-  if (number > 0) {
-    return number; 
-  }
-}
+// function isPositive(number) {
+//   if (number > 0) {
+//     return number; 
+//   }
+// }
 
-  function isMale(name, gender) {
-    if (gender === 'male') {
-      return name + gender; 
-    } 
-  }
-  function filter(arr, ruleFunction) {
-    const output = [];
-    for (let i = 0; i < arr.length; i++) {
-      output.push(ruleFunction(arr[i]));
-    }
+//   function isMale(name, gender) {
+//     if (gender === 'male') {
+//       return name + gender; 
+//     } 
+//   }
+//   function filter(arr, ruleFunction) {
+//     const output = [];
+//     for (let i = 0; i < arr.length; i++) {
+//       output.push(ruleFunction(arr[i]));
+//     }
   
-    return output;
-  }
+//     return output;
+//   }
   
-  console.log(filter([3, -4, 1, 9], isPositive));
+//   console.log(filter([3, -4, 1, 9], isPositive));
 
-  const people = [
-    {name: 'Глеб', gender: 'male'},
-    {name: 'Анна', gender: 'female'},
-    {name: 'Олег', gender: 'male'},
-    {name: 'Оксана', gender: 'female'}
- ];
+//   const people = [
+//     {name: 'Глеб', gender: 'male'},
+//     {name: 'Анна', gender: 'female'},
+//     {name: 'Олег', gender: 'male'},
+//     {name: 'Оксана', gender: 'female'}
+//  ];
  
- console.log(filter(people, isMale));
+//  console.log(filter(people, isMale));
+
+
+
+
+const timer = (deadline) => {
+  const interval = setInterval(() => {
+		let currentDate = new Date;
+	console.log(currentDate);
+	}, 3000);
+  setTimeout(() => {
+    clearInterval(interval);
+    console.log('30 секунд прошло')
+  }, deadline * 3000)
+};
+timer(30);
