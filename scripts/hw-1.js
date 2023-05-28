278137ec822da08655a5320d619b8a14d9aca28b
твое имя < email>
2023-05-28 19:21:43 +0300

    add an index html file

---
+++ scripts/hw-1.js
@@ -0,0 +1,72 @@
+// const people = [
+//   { name: 'Глеб', age: 29 },
+//   { name: 'Анна', age: 17 },
+//   { name: 'Олег', age: 7 },
+//   { name: 'Оксана', age: 47 }
+// ];
+// console.log(people.sort((a, b) => a.age > b.age ? 1 : -1));
+
+function isPositive(number) {
+  if (number > 0) {
+    return number;
+  }
+}
+
+function isMale(user) {
+  if (user.gender === 'male') {
+    return user;
+}
+}
+
+function filter(arr, ruleFunction) {
+  const output = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (ruleFunction(arr[i])) {
+      output.push(ruleFunction(arr[i]));
+    }
+  }
+  return output;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const people = [
+  { name: "Глеб", gender: "male" },
+  { name: "Анна", gender: "female" },
+  { name: "Олег", gender: "male" },
+  { name: "Оксана", gender: "female" },
+];
+console.log(filter(people, isMale));
+
+// const timer = (deadline) => {
+//   const interval = setInterval(() => {
+//    let currentDate = new Date;
+//  console.log(currentDate);
+//  }, 3000);
+
+//   setTimeout(() => {
+//     clearInterval(interval);
+//     console.log('30 секунд прошло')
+//   }, deadline * 3000)
+// };
+
+// timer(30);
+
+// function delayForSecond(callback) {
+//  setTimeout(callback, 1000);
+// }
+
+// delayForSecond(function() {
+//  console.log('Привет, Глеб!');
+// });
+
+// function delayForSecond(cb) {
+// setTimeout(() => {
+//  console.log('Прошла одна секунда');
+//  if(cb) { cb(); }
+//  }, 1000)
+// }
+
+// delayForSecond(function sayHi (name = 'Глеб') {
+//   console.log(`Привет, ${name}!`);
+//   });
